81b36e1c48ee1a42fe905e4cce8d9f0c01186315
Wang Xuerui <idontknw.wang@gmail.com>
2016-09-10 17:41:23 +0800

    mips: support 47-bit VM space as well

--- arch/mips/include/asm/processor.h
+++ arch/mips/include/asm/processor.h
@@ -64,7 +64,11 @@ extern unsigned int vced_count, vcei_count;
  */
 #define TASK_SIZE32	0x7fff8000UL
 #ifdef CONFIG_MIPS_VA_BITS_48
+#ifdef CONFIG_MIPS_VA_BITS_47_COMPAT
+#define TASK_SIZE64     (0x1UL << ((cpu_data[0].vmbits>47)?47:cpu_data[0].vmbits))
+#else
 #define TASK_SIZE64     (0x1UL << ((cpu_data[0].vmbits>48)?48:cpu_data[0].vmbits))
+#endif
 #else
 #define TASK_SIZE64     0x10000000000UL
 #endif
