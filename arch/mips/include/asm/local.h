cab747377e58d7d978f0b898762d0de3670e12f1
WANG Xuerui <git@xen0n.name>
2020-02-11 14:52:24 +0800

    MIPS: local: mark the loongson_war SYNCs as actually unnecessary
    Signed-off-by: WANG Xuerui <git@xen0n.name>

--- arch/mips/include/asm/local.h
+++ arch/mips/include/asm/local.h
@@ -52,7 +52,7 @@ static __inline__ long local_add_return(long i, local_t * l)
 		__asm__ __volatile__(
 		"	.set	push					\n"
 		"	.set	"MIPS_ISA_ARCH_LEVEL"			\n"
-			__SYNC(full, loongson3_war) "			\n"
+			__SYNC(full, loongson3_war_unneeded) "		\n"
 		"1:"	__LL	"%1, %2		# local_add_return	\n"
 		"	addu	%0, %1, %3				\n"
 			__SC	"%0, %2					\n"
@@ -100,7 +100,7 @@ static __inline__ long local_sub_return(long i, local_t * l)
 		__asm__ __volatile__(
 		"	.set	push					\n"
 		"	.set	"MIPS_ISA_ARCH_LEVEL"			\n"
-			__SYNC(full, loongson3_war) "			\n"
+			__SYNC(full, loongson3_war_unneeded) "		\n"
 		"1:"	__LL	"%1, %2		# local_sub_return	\n"
 		"	subu	%0, %1, %3				\n"
 			__SC	"%0, %2					\n"
