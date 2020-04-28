300f2907308e9d0de132805b434998d0b33392d4
WANG Xuerui <git@xen0n.name>
2020-04-28 14:33:57 +0800

    MIPS: Loongson: add optimized xor_block implementation using LoongEXT1
    Signed-off-by: WANG Xuerui <git@xen0n.name>

---
+++ arch/mips/include/asm/xor.h
@@ -0,0 +1,9 @@
+// SPDX-License-Identifier: GPL-2.0
+#ifndef __ASM_MIPS_XOR_H__
+#define __ASM_MIPS_XOR_H__
+
+#if defined(CONFIG_CPU_HAS_LOONGEXT1) && defined(CONFIG_64BIT)
+#include <asm/xor_loongext1.h>
+#endif
+
+#endif
