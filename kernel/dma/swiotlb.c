83851497631268e54c03de90e60973f07522e124
Jianxiong Gao <jxgao@google.com>
2020-12-07 13:42:04 -0800

    swiotlb: Keep offsets when mapping data
    NVMe driver and other applications depend on the data offset
    to operate correctly. Currently when unaligned data is mapped via
    SWIOTLB, the data is mapped as slab aligned with the SWIOTLB.
    
    When booting with swiotlb=force option and using NVMe as interface,
    running mkfs.xfs fails because of the unalignment issue.
    
    We need to make sure the mapped data preserves its offset of
    the orginal address.
    
    Signed-off-by: Jianxiong Gao <jxgao@google.com>
    Acked-by: David Rientjes <rientjes@google.com>
    Signed-off-by: Konrad Rzeszutek Wilk <konrad.wilk@oracle.com>

--- kernel/dma/swiotlb.c
+++ kernel/dma/swiotlb.c
@@ -485,6 +485,12 @@ phys_addr_t swiotlb_tbl_map_single(struct device *hwdev, phys_addr_t orig_addr,
 		    : 1UL << (BITS_PER_LONG - IO_TLB_SHIFT);
 
 	/*
+	 * We need to keep the offset when mapping, so adding the offset
+	 * to the total set we need.
+	 */
+	alloc_size += offset_in_page(orig_addr);
+
+	/*
 	 * For mappings greater than or equal to a page, we limit the stride
 	 * (and hence alignment) to a page size.
 	 */
@@ -567,6 +573,11 @@ found:
 	 */
 	for (i = 0; i < nslots; i++)
 		io_tlb_orig_addr[index+i] = orig_addr + (i << IO_TLB_SHIFT);
+	/*
+	 * When keeping the offset of the original data, we need to advance
+	 * the tlb_addr by the offset of orig_addr.
+	 */
+	tlb_addr += orig_addr & (PAGE_SIZE - 1);
 	if (!(attrs & DMA_ATTR_SKIP_CPU_SYNC) &&
 	    (dir == DMA_TO_DEVICE || dir == DMA_BIDIRECTIONAL))
 		swiotlb_bounce(orig_addr, tlb_addr, mapping_size, DMA_TO_DEVICE);
