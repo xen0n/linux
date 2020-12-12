2911c7c8ae2510846e47ddbce32279affe91cb35
WANG Xuerui <git@xen0n.name>
2020-12-12 16:20:34 +0800

    mm: fix build on ARCH=mips CONFIG_KVM=m
    Fixes: 6cea679f43ca ("mm: memcontrol: account pagetables per node")
    Signed-off-by: WANG Xuerui <git@xen0n.name>

--- mm/memcontrol.c
+++ mm/memcontrol.c
@@ -864,6 +864,7 @@ void __mod_lruvec_page_state(struct page *page, enum node_stat_item idx,
 	lruvec = mem_cgroup_lruvec(memcg, pgdat);
 	__mod_lruvec_state(lruvec, idx, val);
 }
+EXPORT_SYMBOL(__mod_lruvec_page_state);
 
 void __mod_lruvec_kmem_state(void *p, enum node_stat_item idx, int val)
 {
