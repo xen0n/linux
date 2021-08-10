ca3734a777930a07ccc3af1fbe510a2956e98836
WANG Xuerui <git@xen0n.name>
2021-08-10 17:16:15 +0800

    tools: add support for LoongArch builds
    Signed-off-by: WANG Xuerui <git@xen0n.name>

---
+++ tools/arch/loongarch/include/uapi/asm/bitsperlong.h
@@ -0,0 +1,10 @@
+/* SPDX-License-Identifier: GPL-2.0-only */
+
+#ifndef _UAPI_ASM_LOONGARCH_BITSPERLONG_H
+#define _UAPI_ASM_LOONGARCH_BITSPERLONG_H
+
+#define __BITS_PER_LONG (__SIZEOF_POINTER__ * 8)
+
+#include <asm-generic/bitsperlong.h>
+
+#endif /* _UAPI_ASM_LOONGARCH_BITSPERLONG_H */
