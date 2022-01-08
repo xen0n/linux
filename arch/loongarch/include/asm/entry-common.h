d748c0a67619f77128f46ecd392fbc9f0686bcbe
WANG Xuerui <git@xen0n.name>
2022-01-08 23:41:05 +0800

    LoongArch: try to fix ptrace syscall tampering
    see https://github.com/strace/strace/pull/205
    
    Suggested-by: Huacai Chen <chenhuacai@kernel.org>
    Signed-off-by: WANG Xuerui <git@xen0n.name>

--- arch/loongarch/include/asm/entry-common.h
+++ arch/loongarch/include/asm/entry-common.h
@@ -5,9 +5,23 @@
 #include <linux/sched.h>
 #include <linux/processor.h>
 
+#include <asm/syscall.h>
+
 static inline bool on_thread_stack(void)
 {
 	return !(((unsigned long)(current->stack) ^ current_stack_pointer) & ~(THREAD_SIZE - 1));
 }
+
+static inline __must_check int arch_syscall_enter_tracehook(struct pt_regs *regs)
+{
+	int ret = tracehook_report_syscall_entry(regs);
+
+	if (ret)
+		syscall_set_return_value(current, current_pt_regs(), -ENOSYS, 0);
+
+	return ret;
+}
+
+#define arch_syscall_enter_tracehook arch_syscall_enter_tracehook
 
 #endif
