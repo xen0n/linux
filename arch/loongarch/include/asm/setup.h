ac21180e93478abdb2b350ff8a0735a509a5e472
WANG Xuerui <git@xen0n.name>
2022-05-21 08:23:27 +0800

    LoongArch: [DEBUG] add early_printk code from MIPS to aid debugging
    Signed-off-by: WANG Xuerui <git@xen0n.name>

--- arch/loongarch/include/asm/setup.h
+++ arch/loongarch/include/asm/setup.h
@@ -18,4 +18,20 @@ extern void per_cpu_trap_init(int cpu);
 extern void set_handler(unsigned long offset, void *addr, unsigned long len);
 extern void set_merr_handler(unsigned long offset, void *addr, unsigned long len);
 
+#ifdef CONFIG_EARLY_PRINTK
+extern void prom_putchar(char);
+extern void setup_early_printk(void);
+#else
+static inline void prom_putchar(char) {}
+static inline void setup_early_printk(void) {}
+#endif
+
+#ifdef CONFIG_EARLY_PRINTK_8250
+extern void setup_8250_early_printk_port(unsigned long base,
+	unsigned int reg_shift, unsigned int timeout);
+#else
+static inline void setup_8250_early_printk_port(unsigned long base,
+	unsigned int reg_shift, unsigned int timeout) {}
+#endif
+
 #endif /* __SETUP_H */
