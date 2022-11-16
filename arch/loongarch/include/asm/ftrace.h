e5e87cd79c8789f4562154151aa2f743f5b82255
Qing Zhang <zhangqing@loongson.cn>
2022-11-16 16:23:30 +0800

    LoongArch/ftrace: Add basic support
    This patch contains basic ftrace support for LoongArch. Specifically,
    function tracer (HAVE_FUNCTION_TRACER), function graph tracer (HAVE_
    FUNCTION_GRAPH_TRACER) are implemented following the instructions in
    Documentation/trace/ftrace-design.txt.
    
    Use `-pg` makes stub like a child function `void _mcount(void *ra)`.
    Thus, it can be seen store RA and alloc stack before `call _mcount`.
    Find `alloc stack` at first, and then find `store RA`.
    
    Note that the functions in both inst.c and time.c should not be hooked
    with the compiler's -pg option: to prevent infinite self-referencing for
    the former, and to ignore early setup stuff for the latter.
    
    Co-developed-by: Jinyang He <hejinyang@loongson.cn>
    Signed-off-by: Jinyang He <hejinyang@loongson.cn>
    Signed-off-by: Qing Zhang <zhangqing@loongson.cn>
    Signed-off-by: Huacai Chen <chenhuacai@loongson.cn>

---
+++ arch/loongarch/include/asm/ftrace.h
@@ -0,0 +1,20 @@
+/* SPDX-License-Identifier: GPL-2.0 */
+/*
+ * Copyright (C) 2022 Loongson Technology Corporation Limited
+ */
+
+#ifndef _ASM_LOONGARCH_FTRACE_H
+#define _ASM_LOONGARCH_FTRACE_H
+
+#ifdef CONFIG_FUNCTION_TRACER
+
+#define MCOUNT_INSN_SIZE 4		/* sizeof mcount call */
+
+#ifndef __ASSEMBLY__
+#define mcount _mcount
+extern void _mcount(void);
+#endif /* __ASSEMBLY__ */
+
+#endif /* CONFIG_FUNCTION_TRACER */
+
+#endif /* _ASM_LOONGARCH_FTRACE_H */
