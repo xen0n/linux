721d7bfe782412a4fb6c5088fd2e6c03d5bd6ffa
WANG Xuerui <git@xen0n.name>
2023-03-01 09:35:17 +0800

    Revert "drm/loongson: build fix"
    This reverts commit 75afe6edd62bde632e50c2bf09c4ef74491c4f3c.

--- drivers/gpu/drm/loongson/loongson_drv.c
+++ drivers/gpu/drm/loongson/loongson_drv.c
@@ -29,7 +29,6 @@
 #include <drm/drm_aperture.h>
 #include <drm/drm_atomic_helper.h>
 #include <drm/drm_crtc_helper.h>
-#include <drm/drm_fbdev_generic.h>
 #include <drm/drm_probe_helper.h>
 #include <drm/drm_gem_dma_helper.h>
 #include <drm/drm_gem_framebuffer_helper.h>
